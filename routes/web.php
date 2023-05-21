8878d3184d2c36982a68c3c8dbe06c2062ba9108
Karl Murray <kmurray@foxworldtravel.com>
2023-05-21 10:37:43 -0500

    Add Duster / Lint Files

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
