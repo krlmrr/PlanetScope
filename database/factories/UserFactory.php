7dd53418884044ffc137702506510e9a6774333f
Karl Murray <karl@karlm.dev>
2024-01-27 12:20:30 -0600

    Add Laravel Pulse

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,7 +13,7 @@ class UserFactory extends Factory
     public function configure(): static
     {
         return $this->afterMaking(function (User $user) {
-//            $user->currentTeam = $user->allTeams()->first()->id;
+            $user->currentTeam = $user->allTeams()->first()->id;
         })->afterCreating(function (User $user) {
             $user->currentTeam = $user->allTeams()->first()->id;
         });
@@ -25,7 +25,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'),
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
--- tests/Feature/BrowserSessionsTest.php
+++ tests/Feature/BrowserSessionsTest.php
@@ -6,9 +6,8 @@ use function Pest\Laravel\actingAs;
 use function Pest\Laravel\delete;
 
 test('other browser sessions can be logged out', function () {
-    actingAs($user = User::factory()->create());
-
-    delete('/user/other-browser-sessions', [
-        'password' => 'password',
-    ])->assertSessionHasNoErrors();
+    actingAs(User::factory()->create())
+        ->delete('/user/other-browser-sessions', [
+            'password' => 'password',
+        ])->assertSessionHasNoErrors();
 });
